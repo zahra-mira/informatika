4bd5e758b36acdc7662468ca2477307aa2aaff66
zahra-mira <zahra.almiralatif@gmail.com>
2025-05-27 09:04:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,8 @@ st.write(
     "Macam-macam Hewan:"
 )
 st.image("IMG_0815.jpeg", width=200)
-st.write("1. Hewan Karnivora adalah hewan yang memakan daging hewan lainnya")
+st.write("1. Hewan Karnivora adalah hewan yang memakan daging hewan lainnya. Contoh hewan karnivora adalah singa, harimau, buaya, dan lainnya")
+st.image("IMG_0816.jpeg", width=200)
+st.write("2. Hewan Herbivora adalah hewan yang memakan tumbuhan seperti daun dan biji. Contoh hewan herbivora adalah domba, sapi, dan lainnya")
+st.image("IMG_0817.jpeg", width=200)
+st.write("3. Hewan Omnivora adalah hewan yang memakan daging dan tumbuhan. Contoh hewan omnivora adalah burung gagak, babi, dan lainnya")
