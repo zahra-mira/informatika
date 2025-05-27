1ac78628dc55ea7e0f5802a90a86fbd1435b9aa5
zahra-mira <zahra.almiralatif@gmail.com>
2025-05-27 08:53:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.title("Selamat Datang di Web")
+st.title("Selamat Datang")
 st.write(
     "Macam-macam Hewan"
 )
 st.image("IMG_0815.jpeg", width=200)
+st.write("Hewan Karnivora")
