478365e6a7b2e94e428b9d5608d8fc8b47934f4c
zahra-mira <zahra.almiralatif@gmail.com>
2025-05-27 09:31:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,13 @@ st.image("IMG_0816.jpeg", width=200)
 st.write("2. Hewan Herbivora adalah hewan yang memakan tumbuhan seperti daun dan biji. Contoh hewan herbivora adalah domba, sapi, dan lainnya")
 st.image("IMG_0817.jpeg", width=200)
 st.write("3. Hewan Omnivora adalah hewan yang memakan daging dan tumbuhan. Contoh hewan omnivora adalah burung gagak, babi, dan lainnya")
+
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
+    
